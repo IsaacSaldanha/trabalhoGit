922502654273c7cedc0afa9050b9ecf3c1bf8192
Isaac Saldanha Araujo <isaacsaldanha01@gmail.com>
2025-09-03 23:43:11 -0300

    feature: trabalho concluido

---
+++ src/TrabalhoGitPatrick.java
@@ -0,0 +1,11 @@
+public class TrabalhoGitPatrick {
+    public static void main(String[] args) {
+        System.out.println("git branch isaac-saldanha");
+        System.out.println("git status");
+        System.out.println("git add.");
+        System.out.println("git status");
+        System.out.println("git commit -m \"feature: trabalho concluido");
+        System.out.println("git push -u origin isaac-saldanha");
+
+    }
+}
